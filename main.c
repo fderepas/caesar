09244f0cd9b74830bfe9c5957ef19af983f8dfc7
Fabrice Derepas <derepas@trust-in-soft.com>
2018-02-18 14:32:02 +0100

    Step 4

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ void gen_test(int user_shift, int len, char *str)
     res1 = encrypt(str, len, user_shift);
     printf("Result: %s\n", res1);
 
-    printf("Decrypt text '%s'\n", res1);
-    res2 = decrypt(res1, len, user_shift);
+    printf("Decrypt text '%s'\n", str);
+    res2 = decrypt(str, len, user_shift);
     printf("Result: %s\n", res2);
 
     free(res1);
@@ -31,6 +31,9 @@ int main(void)
     printf("\nTest 2: Shift with a positive input\n");
     gen_test(7, len, str);
     printf("\nTest 3: Shift with all possible input\n");
+    gen_test(user_shift, len, str);
+    tis_make_unknown(str, len - 1);
+    printf("\nTest 4: All possible input\n");
     gen_test(user_shift, len, str);
 
     return 0;
