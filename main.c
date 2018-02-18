734ddda2dad677065492a85d48f260e62df96566
Fabrice Derepas <derepas@trust-in-soft.com>
2018-02-18 14:30:00 +0100

    Step 2

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <caesar.h>
+#include <tis_builtin.h>
 
 void gen_test(int user_shift, int len, char *str)
 {
@@ -22,11 +23,15 @@ int main(void)
 {
     char str[] = "People of Earth, your attention please";
     int len = sizeof(str) / sizeof(str[0]);
+    int user_shift;
+    tis_make_unknown((char *)&user_shift, sizeof(int));
 
     printf("Test 1: Shift with a negative input\n");
     gen_test(-3, len, str);
     printf("\nTest 2: Shift with a positive input\n");
     gen_test(7, len, str);
+    printf("\nTest 3: Shift with all possible input\n");
+    gen_test(user_shift, len, str);
 
     return 0;
 }
