97ca8bd4df0d290b0aaf44e0eafedb66fabab79a
Fabrice Derepas <derepas@trust-in-soft.com>
2018-02-19 18:29:56 +0100

    coherent free calls

--- main.c
+++ main.c
@@ -8,27 +8,27 @@ void gen_test(int user_shift, int len, char *str)
 
     printf("Encrypt text '%s'\n", str);
     res1 = encrypt(str, len, user_shift);
-    if (res1==NULL) return;
-    printf("Result: %s\n", res1);
-
-    printf("Decrypt text '%s'\n", res1);
-    res2 = decrypt(res1, len, user_shift);
-    if (res2==NULL) return;
-    printf("Result: %s\n", res2);
-
+    if (res1) {
+      printf("Result: %s\n", res1);
+      
+      printf("Decrypt text '%s'\n", res1);
+      res2 = decrypt(res1, len, user_shift);
+      if (res2) {
+        printf("Result: %s\n", res2);
+        free(res2);
+      }
+    }
     free(res1);
-    free(res2);
 }
 
-int main(void)
-{
-    char str[] = "People of Earth, your attention please";
-    int len = sizeof(str) / sizeof(str[0]);
-
-    printf("Test 1: Shift with a negative input\n");
-    gen_test(-3, len, str);
-    printf("\nTest 2: Shift with a positive input\n");
-    gen_test(7, len, str);
-
-    return 0;
+int main(void) {
+  char str[] = "People of Earth, your attention please";
+  int len = sizeof(str) / sizeof(str[0]);
+  
+  printf("Test 1: Shift with a negative input\n");
+  gen_test(-3, len, str);
+  printf("\nTest 2: Shift with a positive input\n");
+  gen_test(7, len, str);
+  
+  return 0;
 }
