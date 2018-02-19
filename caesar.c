44fe87cf4f43f84ed08711bb7fe8ce2bf97fda76
Fabrice Derepas <derepas@trust-in-soft.com>
2018-02-19 18:31:08 +0100

    coherent string manip

--- caesar.c
+++ caesar.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int absolute_int(int x) {
-  int abs_x;
-  if (x < 0)
-    abs_x = -x;
-  else
-    abs_x = x;
-  return abs_x;
+int absolute_int(int x)
+{
+    int abs_x;
+    if (x < 0)
+      abs_x = -x;
+    else
+      abs_x = x;
+    return abs_x;
 }
 
 char *encrypt(char *str, int len, int d) {
   int shift = absolute_int(d);
   int i = 0;
   char *buf = (char *)malloc(len);
-
+  
   if (buf) {
-    while (*str) {
+    while (i<len-1) {
       /* Spaces are not encrypted. */
-      buf[i] = *str == ' ' ? ' ' : *str + (char)shift;
+      buf[i] = str[i] == ' ' ? ' ' : str[i] + (char)shift;
       i++;
-      str++;
     }
     buf[i] = '\0';
   }
-  return buf;
+    return buf;
 }
 
 char *decrypt(char *str, int len, int d) {
@@ -33,11 +33,10 @@ char *decrypt(char *str, int len, int d) {
   char *buf = (char *)malloc(len);
   
   if (buf) {
-    while (*str) {
+    while (i<len-1) {
       /* Spaces are not decrypted. */
-      buf[i] = *str == ' ' ? ' ' : *str - (char)shift;
+      buf[i] = str[i] == ' ' ? ' ' : str[i] - (char)shift;
       i++;
-      str++;
     }
     buf[i] = '\0';
   }
