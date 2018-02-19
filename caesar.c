c8e5fd1f5658df04841a700ab63e227a995551dd
Fabrice Derepas <derepas@trust-in-soft.com>
2018-02-19 18:14:07 +0100

    homogenous style with step 4

--- caesar.c
+++ caesar.c
@@ -1,46 +1,45 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int absolute_int(int x)
-{
-    int abs_x;
-    if (x < 0)
-      abs_x = -x;
-    else
-      abs_x = x;
-    return abs_x;
+int absolute_int(int x) {
+  int abs_x;
+  if (x < 0)
+    abs_x = -x;
+  else
+    abs_x = x;
+  return abs_x;
 }
 
-char *encrypt(char *str, int len, int d)
-{
-    int shift = absolute_int(d);
-    int i = 0;
-    char *buf = (char *)malloc(len);
-    if (buf==NULL) return NULL;
-    
+char *encrypt(char *str, int len, int d) {
+  int shift = absolute_int(d);
+  int i = 0;
+  char *buf = (char *)malloc(len);
+
+  if (buf) {
     while (*str) {
-        /* Spaces are not encrypted. */
-        buf[i] = *str == ' ' ? ' ' : *str + (char)shift;
-        i++;
-        str++;
+      /* Spaces are not encrypted. */
+      buf[i] = *str == ' ' ? ' ' : *str + (char)shift;
+      i++;
+      str++;
     }
     buf[i] = '\0';
-    return buf;
+  }
+  return buf;
 }
 
-char *decrypt(char *str, int len, int d)
-{
-    int shift = absolute_int(d);
-    int i = 0;
-    char *buf = (char *)malloc(len);
-    if (buf==NULL) return NULL;
-    
+char *decrypt(char *str, int len, int d) {
+  int shift = absolute_int(d);
+  int i = 0;
+  char *buf = (char *)malloc(len);
+  
+  if (buf) {
     while (*str) {
-        /* Spaces are not decrypted. */
-        buf[i] = *str == ' ' ? ' ' : *str - (char)shift;
-        i++;
-        str++;
+      /* Spaces are not decrypted. */
+      buf[i] = *str == ' ' ? ' ' : *str - (char)shift;
+      i++;
+      str++;
     }
     buf[i] = '\0';
-    return buf;
+  }
+  return buf;
 }
--- main.c
+++ main.c
@@ -3,36 +3,34 @@
 #include <caesar.h>
 #include <tis_builtin.h>
 
-void gen_test(int user_shift, int len, char *str)
-{
-    char *res1, *res2;
+void gen_test(int user_shift, int len, char *str) {
+  char *res1, *res2;
+  
+  printf("Encrypt text '%s'\n", str);
+  res1 = encrypt(str, len, user_shift);
+  if (!res1) return;
 
-    printf("Encrypt text '%s'\n", str);
-    res1 = encrypt(str, len, user_shift);
-    if (!res1) return;
-
-    printf("Decrypt text '%s'\n", res1);
-    res2 = decrypt(res1, len, user_shift);
-    if (!res2) return;
-
-    free(res1);
-    free(res2);
+  printf("Decrypt text '%s'\n", res1);
+  res2 = decrypt(res1, len, user_shift);
+  if (!res2) return;
+  
+  free(res1);
+  free(res2);
 }
 
-int main(void)
-{
-    char str[] = "People of Earth, your attention please";
-    int len = sizeof(str) / sizeof(str[0]);
-    int user_shift;
-    tis_make_unknown((char *)&user_shift, sizeof(int));
-    str[len-1]=0;
-
-    printf("Test 1: Shift with a negative input\n");
-    gen_test(-3, len, str);
-    printf("\nTest 2: Shift with a positive input\n");
-    gen_test(7, len, str);
-    printf("\nTest 3: Shift with all possible input\n");
-    if (user_shift != INT_MIN )
-      gen_test(user_shift, len, str);
-    return 0;
+int main(void) {
+  char str[] = "People of Earth, your attention please";
+  int len = sizeof(str) / sizeof(str[0]);
+  int user_shift;
+  tis_make_unknown((char *)&user_shift, sizeof(int));
+  str[len-1]=0;
+  
+  printf("Test 1: Shift with a negative input\n");
+  gen_test(-3, len, str);
+  printf("\nTest 2: Shift with a positive input\n");
+  gen_test(7, len, str);
+  printf("\nTest 3: Shift with all possible input\n");
+  if (user_shift != INT_MIN )
+    gen_test(user_shift, len, str);
+  return 0;
 }
